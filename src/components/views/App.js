97d74bfb513ddd8a1c3cac300a4b8d4b408504c7
Francis <git@francisleigh.co.uk>
2018-07-23 23:10:49 +0100

    App

---
+++ src/components/views/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Basket from '../Basket'
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Basket />
+      </div>
+    )
+  }
+}
+
+export default App

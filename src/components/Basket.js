1a730d71c74b30645f1058f6e1c7379b0da159b8
Francis <git@francisleigh.co.uk>
2018-07-23 23:14:38 +0100

    basket component

---
+++ src/components/Basket.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { clearQuantities } from '../store/actions'
+
+import Item from './Item'
+import BasketTotal from './Total'
+const Basket = ({ products, clearQuantities }) => {
+  return (
+    <div className="basket">
+      <div className="basket flex--wider basket flex--padd">
+        {products.map((props) => (
+            <Item key={`product-${props.id}`} {...props} />
+          )
+        )}
+      </div>
+      <div className="basket__footer flex flex--row flex--center">
+        <h1 className="flex flex--fit">
+          £<BasketTotal />
+        </h1>
+        <div className="flex flex--fit flex--row">
+          <button className="button" onClick={() => clearQuantities()}>Clear</button>
+          <button className="button button--primary">Checkout ></button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  products: state
+})
+
+const mapDispatchToProps = dispatch => ({
+  clearQuantities: () => dispatch(clearQuantities())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Basket)

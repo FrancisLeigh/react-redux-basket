6ca0cfca2118b8e4b5e24710814e1b72e85a103b
Francis <git@francisleigh.co.uk>
2018-07-24 07:47:08 +0100

    hidden label for input

--- src/components/QuantityController.js
+++ src/components/QuantityController.js
@@ -3,7 +3,10 @@ import { connect } from 'react-redux'
 import { changeQuantity } from '../store/actions'
 
 const QuantityController = ({ id, quantity, changeQuantity }) => (
-  <input className="quantity-ctrl" type="number" value={quantity} onChange={(e) => changeQuantity(id, e.target.value)} />
+  <div>
+    <label className="accessible" htmlFor={id}>Quantity</label>
+    <input id={id} className="quantity-ctrl" type="number" value={quantity} onChange={(e) => changeQuantity(id, e.target.value)} />
+  </div>
 )
 
 const mapDispatchToProps = dispatch => ({

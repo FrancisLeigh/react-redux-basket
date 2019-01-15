44221706dbcc15e9ea3a179cba97ac4ba700952b
FrancisLeigh <git@francisleigh.co.uk>
2019-01-15 14:36:50 +0000

    basket meta discount shape now Object instead of null

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,14 +3,14 @@ import { combineReducers } from 'redux'
 import actions from 'Constants/actions'
 import ItemsReducer from 'Store/reducers/items'
 
-export const DefaultMeta = { discount: null }
+export const DefaultMeta = { discount: {} }
 
 const BasketMeta = (state = DefaultMeta, action) => {
   switch (action.type) {
     case actions.APPLY_DISCOUNT:
       return { ...state, discount: action.payload }
     case actions.REMOVE_DISCOUNT:
-      return { ...state, discount: null }
+      return { ...state, discount: {} }
     default:
       return state
   }
